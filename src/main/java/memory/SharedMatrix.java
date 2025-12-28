125c1f1331176099fcca969a9c9e376dc956b8c4
Ronenkh <ronenavr@post.bgu.ac.il>
2025-12-28 15:37:19 +0200

    Added SharedMatrix and Main.Java args parsing

--- src/main/java/memory/SharedMatrix.java
+++ src/main/java/memory/SharedMatrix.java
@@ -3,17 +3,43 @@ package memory;
 public class SharedMatrix {
 
     private volatile SharedVector[] vectors = {}; // underlying vectors
+    private volatile VectorOrientation orientation = VectorOrientation.ROW_MAJOR;
+
 
     public SharedMatrix() {
         // TODO: initialize empty matrix
+        this.vectors = new SharedVector[0];
     }
 
     public SharedMatrix(double[][] matrix) {
         // TODO: construct matrix as row-major SharedVectors
+        loadRowMajor(matrix);
+
     }
 
     public void loadRowMajor(double[][] matrix) {
         // TODO: replace internal data with new row-major matrix
+
+        if( matrix == null){
+            throw new IllegalArgumentException("matrix is null");
+        }
+        SharedVector[] newVectors = new SharedVector[matrix.length];
+
+        for(int i = 0; i < matrix.length; i++) {
+            if (matrix[i] == null) {
+                throw new IllegalArgumentException("row " + i + " is null");
+            }
+            double[] row = new double[matrix[i].length];
+            for (int j = 0; j < matrix[i].length; j++) {
+                row[j] = matrix[i][j];
+            }
+
+
+            newVectors[i] = new SharedVector(row, VectorOrientation.ROW_MAJOR);
+        }
+
+        this.vectors = newVectors;
+        this.orientation = VectorOrientation.ROW_MAJOR;
     }
 
     public void loadColumnMajor(double[][] matrix) {
@@ -27,32 +53,47 @@ public class SharedMatrix {
 
     public SharedVector get(int index) {
         // TODO: return vector at index
-        return null;
+        return vectors[index];
     }
 
     public int length() {
         // TODO: return number of stored vectors
-        return 0;
+        return vectors.length;
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return orientation
-        return null;
+        return orientation;
     }
 
     private void acquireAllVectorReadLocks(SharedVector[] vecs) {
         // TODO: acquire read lock for each vector
+        for (SharedVector v : vecs) {
+            v.readLock();
+        }
     }
 
     private void releaseAllVectorReadLocks(SharedVector[] vecs) {
         // TODO: release read locks
+
+        for (SharedVector v : vecs) {
+            v.readUnlock();
+        }
+
     }
 
     private void acquireAllVectorWriteLocks(SharedVector[] vecs) {
         // TODO: acquire write lock for each vector
+        for (SharedVector v : vecs) {
+            v.writeLock();
+        }
     }
 
     private void releaseAllVectorWriteLocks(SharedVector[] vecs) {
         // TODO: release write locks
+
+        for (SharedVector v : vecs) {
+            v.writeUnlock();
+        }
     }
 }
--- src/main/java/spl/lae/Main.java
+++ src/main/java/spl/lae/Main.java
@@ -6,5 +6,33 @@ import parser.*;
 public class Main {
     public static void main(String[] args) throws IOException {
       // TODO: main
+
+        if(args.length != 3){
+            System.err.println("Not enough args");
+        }
+
+        int numOfThreads = Integer.parseInt(args[0]);
+        String inputPath = args[1];
+        String outputPath = args[2];
+
+        try{
+            InputParser inputParser = new InputParser();
+            ComputationNode root = inputParser.parse(inputPath);
+
+            root.associativeNesting();
+
+            LinearAlgebraEngine engine = new LinearAlgebraEngine(numOfThreads);
+            ComputationNode result = engine.run(root);
+            OutputWriter.write(result.getMatrix(), outputPath);
+
+
+        }catch (Exception e) {
+            try {
+                OutputWriter.write(e.getMessage(), outputPath);
+            } catch (IOException io) {
+                // last-resort failure
+                io.printStackTrace();
+            }
+        }
     }
 }
